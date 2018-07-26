a0a0a8d149cf10fd0b52418968f392382a4135f6
Bernard Duggan <bernard@hippware.com>
2018-07-26 13:34:30 +1000

    Add support for Absinthe continuations

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Absinthe.Phoenix.Mixfile do
   defp deps do
     [
       {:absinthe_plug, "~> 1.4.0"},
-      {:absinthe, "~> 1.4.0 or ~> 1.5.0-alpha.0"},
+      #{:absinthe, "~> 1.4.0 or ~> 1.5.0-alpha.0"},
+      {:absinthe, github: "hippware/absinthe", branch: "defer-directive", override: true},
       {:decimal, "~> 1.0"},
       {:phoenix, "~> 1.2"},
       {:phoenix_pubsub, "~> 1.0"},
